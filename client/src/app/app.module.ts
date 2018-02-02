31052a9da6d39560996a72e2425080ab080c297d
vip1all <vip.all@hotmail.com>
2018-02-02 11:10:17 +0100

    Configured ng4 router.
    Created admin and login component.
    TODO: Create auth service and login guard to see graphs and admin guard
    for admin component!

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
 
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes, {enableTracing: true}),
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
